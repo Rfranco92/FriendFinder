c4b860388273539171142bf43e536d7c607d2eab
Ryan Franco <rfranco92@gmail.com>
2017-10-10 19:04:43 -0400

    Fixed a few errors

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// These data sources hold arrays of information on friends, etc.
 // ===============================================================================
 
 var friends = require("../data/friends");
@@ -26,8 +26,8 @@ module.exports = function(app) {
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
+  // (ex. User fills out a form of questions
+  // Then the server saves the data to the friends array and shows who is most compatible with that user.)
   // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
